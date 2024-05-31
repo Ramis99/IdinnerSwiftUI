76254256aa98b5cda915b32469cf4baf70947f54
Ramis99 <68399319+Ramis99@users.noreply.github.com>
2024-05-31 12:19:47 -0600

    Feat(Main View) Creation of project and dynamic List
    The project was created and the dynamic list functionality was added

---
+++ iDinnerSwiftUI/iDinnerSwiftUI/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  iDinnerSwiftUI
+//
+//  Created by Ramiro Gutierrez de la Paz on 31/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    var body: some View {
+        NavigationStack {
+            List(content: {
+                ForEach(menu) { section in
+                    Section(section.name) {
+                        ForEach(section.items) { items in
+                            Text(items.name)
+                        }
+                    }
+                    
+                }
+            })
+            .navigationTitle("Menu")
+            .listStyle(.automatic)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iDinnerSwiftUI/iDinnerSwiftUI/iDinnerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDinnerSwiftUIApp.swift
+//  iDinnerSwiftUI
+//
+//  Created by Ramiro Gutierrez de la Paz on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct iDinnerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
